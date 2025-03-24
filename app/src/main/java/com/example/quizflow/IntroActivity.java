bac9439f56945d40767253868c33a74f83710ebf
Vix <148944804+Vi021@users.noreply.github.com>
2025-03-24 21:26:42 +0700

    init basic activities + Intro UI

---
+++ app/src/main/java/com/example/quizflow/IntroActivity.java
@@ -0,0 +1,32 @@
+package com.example.quizflow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            finish();
+        }, 2000);
+    }
+}
