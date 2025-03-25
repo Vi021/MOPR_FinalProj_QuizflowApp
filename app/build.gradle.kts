3b4540b1c9b2deb0835d52510aa9ffb362edd9e4
Vix <148944804+Vi021@users.noreply.github.com>
2025-03-25 14:48:08 +0700

    HomeFragment UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -37,7 +40,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // added dependencies
     implementation(libs.circleimageview)
+    implementation(libs.glide)
+    implementation(libs.glideCompiler)
+    implementation(libs.glideTransformations)
+    implementation(libs.chipNavbar)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
