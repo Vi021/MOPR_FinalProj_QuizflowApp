60716842f00bc06b00dfbedc8aa09ab4c998115b
LacDuong <lacduongldg212@gmail.com>
2025-03-26 01:00:53 +0700

    Add Question UI

--- app/src/main/java/com/example/quizflow/activities/QuestionActivity.java
+++ app/src/main/java/com/example/quizflow/activities/QuestionActivity.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quizflow.R;
 
-public class RankingFragment extends AppCompatActivity {
+public class QuestionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.fragment_ranking);
+        setContentView(R.layout.activity_question);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
