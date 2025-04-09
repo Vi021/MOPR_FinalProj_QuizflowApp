d39e5c74af42b0256d6f9a2890858f015df1061f
Vi021 <148944804+Vi021@users.noreply.github.com>
2025-04-09 22:35:03 +0700

    Signin UI

--- app/src/main/java/com/example/quizflow/activities/SigninActivity.java
+++ app/src/main/java/com/example/quizflow/activities/SigninActivity.java
@@ -1,8 +1,17 @@
 package com.example.quizflow.activities;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -22,5 +31,36 @@ public class SigninActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        SpannableString spannable = getSpannableString();
+
+        TextView txt_signUp = findViewById(R.id.txt_signup);
+        txt_signUp.setText(spannable);
+        txt_signUp.setMovementMethod(new android.text.method.LinkMovementMethod()); // enable click event
+        txt_signUp.setHighlightColor(Color.TRANSPARENT);
+    }
+
+    // for partially clickable, underlined, different color text in a string, in this case: "New to Quizflow? >>Sign up now!<<"
+    @NonNull
+    private SpannableString getSpannableString() {
+        SpannableString spannable = new SpannableString("New to Quizflow? Sign up now!");
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent intent = new Intent(SigninActivity.this, SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(true);      // ensure underline
+                ds.setColor(getResources().getColor(R.color.xanh_ngoc));    // to change text color
+            }
+        };
+
+        spannable.setSpan(clickableSpan, 16, spannable.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return spannable;
     }
 }
